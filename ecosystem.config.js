4bc80c09a8abaa3beb9cd7c43a0fe2f5dd3a0fca
Alan Gregory <aragogue16@gmail.com>
2026-02-12 12:52:41 -0300

    update pm2 scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'stake-check',
-        script: './dist/services/sync.js',
+        script: './dist/src/services/sync.js',
         instances: 1,
         autorestart: true,
         watch: false,
